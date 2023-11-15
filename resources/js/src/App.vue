81a8cd4f81c13df96cbaec47e0bc45547bad14ab
Mikhail Protasevich <m1nyasha@yandex.ru>
2023-11-15 12:59:05 +0300

    Исправлена ошибка с импортом компонента PrimaryHeader

--- resources/js/src/App.vue
+++ resources/js/src/App.vue
@@ -7,7 +7,7 @@
 
 <script lang="ts">
 	import { defineComponent } from 'vue';
-	import PrimaryHeader from '/components/PrimaryHeader.vue';
+	import PrimaryHeader from './components/PrimaryHeader.vue';
 
 	export default defineComponent({
 		components: {
